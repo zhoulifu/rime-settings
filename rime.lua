3ee8c8b9669ccba7e996348222018097726f95a5
zhoulifu <zlf369777684@gmail.com>
2024-01-23 13:44:09 +0800

    emoji

--- rime.lua
+++ rime.lua
@@ -1,7 +1,9 @@
 ---@diagnostic disable: undefined-global, lowercase-global
-function opencc_emoji_filter(input)
+function opencc_emoji_filter(input, env)
+  local on = env.engine.context:get_option("emoji")
+
   for cand in input:iter() do
-    if cand:get_dynamic_type() == "Shadow" then
+    if not on or cand:get_dynamic_type() == "Shadow" then
       yield(cand)
     end
   end
